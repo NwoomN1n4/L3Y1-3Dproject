f5a574b36f6964b33d2aad8b93d1045206ba6cdf
meloluna <pol23006232@heartofyorkshire.ac.uk>
2025-01-23 12:50:03 +0000

    ?

---
+++ L3Y1- 3Dproject/Assets/Scripts/PlayerCollider.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollider : MonoBehaviour
+{
+    Vector3 lastPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPos = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("KillTrigger"))
+        {
+            transform.position = lastPos;
+        }
+    }
+}
